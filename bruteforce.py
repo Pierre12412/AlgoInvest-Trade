65c4e4be8784bc08054e9d8cd0311d8fbe9bda71
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-14 16:08:36 +0200

    bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,8 +1,20 @@
+import csv
+
 possibilities = []
 numbers = [1,5,4,2,3,6,11,22]
+actions = []
+costs = []
+rewards = []
 
 
-def all_sums(numbers, max, liste):
+with open('actions.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar=',')
+    for row in reader:
+        if row[0] != '' and row[1] != 'price':
+            actions.append(row)
+
+
+def all_sums(numbers, max, liste=[]):
     somme = sum(liste)
     if somme <= max: 
         possibilities.append(liste)
@@ -15,6 +27,25 @@ def all_sums(numbers, max, liste):
         rest = numbers[i+1:]
         all_sums(rest, max, liste + [number]) 
 
-all_sums(numbers=numbers,max=10,liste=[])
+# all_sums(numbers=numbers,max=10,liste=[])
+
+for action in actions:
+    costs.append(int(action[1]))
+    rewards.append((int(action[1])*int(action[2]))/100)
+
+all_sums(costs,500)
+
+for ind, possibility in enumerate(possibilities):
+    for index, item in enumerate(possibility):
+        for act_index, action in enumerate(actions):
+            if int(action[1]) == item:
+                possibilities[ind][index] = rewards[act_index]
+                break
+
 for possibility in possibilities:
-    print(possibility)
+    sums = []
+    sums.append(sum(possibility))
+sums = sums.sort(reverse=True)
+
+for sum in sums:
+    print(str(sum) + '\n')
