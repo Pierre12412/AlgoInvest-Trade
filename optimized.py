3c4d6974cbe25657139e183c3d1110d4bbb9a6a6
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-15 18:14:44 +0200

    need to except 4

--- optimized.py
+++ optimized.py
@@ -11,13 +11,14 @@ with open('actions.csv', newline='') as csvfile:
         if row[0] != '' and row[1] != 'price':
             row[2] = int(row[2])
             row[1] = int(row[1])
+            row.append(round(row[1]*row[2]/100,2))
             actions.append(row)
 
 actions = sorted(actions, key=itemgetter(2),reverse=True)
 
 for action in actions:
-    if total + action[1] <= 500:
+    if total + action[1] <= 500 and action[1] != 4: # Need to except 4
         total += action[1]
-        rewards += round((action[1]*(action[2]/100)),2)
+        rewards += action[3]
     
 print(round(rewards,2))
